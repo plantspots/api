4a62d4e9fe6084ad2efb021f3fcf3a4c64efba3e
Seyon Satheesh <seyon.satheesh@gmail.com>
2025-03-01 16:19:35 -0500

    Fixed Request Data Parsing

--- api/views.py
+++ api/views.py
@@ -15,8 +15,8 @@ class Login(APIView):
     """
 
     def post(self, request, format=None):
-        username = request.query_params.get("username", "")
-        password = request.query_params.get("password", "")
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
 
         if username == "" or password == "":
             return Response({"error_message": "Please Fill All Fields"}, status=status.HTTP_400_BAD_REQUEST)
@@ -38,10 +38,10 @@ class CreateAccount(APIView):
     """
 
     def post(self, request, format=None):
-        username = request.query_params.get("username", "").strip()
-        password = request.query_params.get("password", "").strip()
-        email = request.query_params.get("email", "").strip()
-        phone = request.query_params.get("phone", "").strip()
+        username = request.data.get("username", "").strip()
+        password = request.data.get("password", "").strip()
+        email = request.data.get("email", "").strip()
+        phone = request.data.get("phone", "").strip()
 
         if username == "" or password == "" or email == "" or phone == "":
             return Response({"error_message": "Please Fill All Fields"}, status=status.HTTP_400_BAD_REQUEST)
@@ -66,7 +66,7 @@ class RequestData(APIView):
     """
 
     def get(self, request, format=None):
-        hash = request.query_params.get("hash", "").strip()
+        hash = request.data.get("hash", "").strip()
 
         if hash == "":
             return Response({"error_message": "Invalid User"}, status=status.HTTP_400_BAD_REQUEST)
@@ -82,13 +82,13 @@ class RequestData(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
-        hash = request.query_params.get("hash", "").strip()
-        title = request.query_params.get("title", "").strip()
-        description = request.query_params.get("description", "").strip()
-        type = request.query_params.get("type", "").strip() # 0 - Land, 1 - Plants
-        email_contact = request.query_params.get("email_contact", "").strip()
-        phone_contact = request.query_params.get("phone_contact", "").strip()
-        images = request.query_params.get("images", "").strip() # URL-safe BASE64 images separated by * characters
+        hash = request.data.get("hash", "").strip()
+        title = request.data.get("title", "").strip()
+        description = request.data.get("description", "").strip()
+        type = request.data.get("type", "").strip() # 0 - Land, 1 - Plants
+        email_contact = request.data.get("email_contact", "").strip()
+        phone_contact = request.data.get("phone_contact", "").strip()
+        images = request.data.get("images", "").strip() # URL-safe BASE64 images separated by * characters
 
         if hash == "" or title == "" or description == "" or type == "" or email_contact == "" or phone_contact == "":
             return Response({"error_message": "Please Fill All Fields"}, status=status.HTTP_400_BAD_REQUEST)
@@ -110,52 +110,52 @@ class RequestData(APIView):
         serializer = RequestSerializer(request)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def login(request):
-    """
-    Login user.
-    """
-    if request.method == 'GET':
-        all_requests = Request.objects.all()
-        serializer = RequestSerializer(all_requests, many=True)
-        return Response(serializer.data)
+# @api_view(['POST'])
+# def login(request):
+#     """
+#     Login user.
+#     """
+#     if request.method == 'GET':
+#         all_requests = Request.objects.all()
+#         serializer = RequestSerializer(all_requests, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = RequestSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'POST':
+#         serializer = RequestSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def create_account(request):
-    """
-    Create user.
-    """
+# @api_view(['POST'])
+# def create_account(request):
+#     """
+#     Create user.
+#     """
 
-    data = request.data.copy()
-    data["password"] = make_password(request.data["password"])
-    data["hash"] = generate_unique_hash(User.objects.values_list("hash", flat=True))
+#     data = request.data.copy()
+#     data["password"] = make_password(request.data["password"])
+#     data["hash"] = generate_unique_hash(User.objects.values_list("hash", flat=True))
 
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid() and is_valid_email(request.data["email"]) and is_valid_phone(request.data["email"]):
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     serializer = UserSerializer(data=request.data)
+#     if serializer.is_valid() and is_valid_email(request.data["email"]) and is_valid_phone(request.data["email"]):
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def requests(request):
-    """
-    List all requests, or create a new request.
-    """
-    if request.method == 'GET':
-        all_requests = Request.objects.all()
-        serializer = RequestSerializer(all_requests, many=True)
-        return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def requests(request):
+#     """
+#     List all requests, or create a new request.
+#     """
+#     if request.method == 'GET':
+#         all_requests = Request.objects.all()
+#         serializer = RequestSerializer(all_requests, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = RequestSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'POST':
+#         serializer = RequestSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
